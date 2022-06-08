6c33a8b2bbabe06cb316ad0c3b63ddb3df3186a6
SouL-H <mhmtaydinw@gmail.com>
2022-06-08 11:25:28 +0300

    webTable added.

--- main.go
+++ main.go
@@ -1,50 +1,52 @@
 package main
 
 import (
-	"fmt"
-	helper "loginApi/helpers"
-	"net/http"
+	//"fmt"
+	//helper "loginApi/helpers"
+	. "loginApi/WebServer"
+	//"net/http"
 )
 
 func main() {
-	uName, email, pass := "", "", ""
-	mux := http.NewServeMux()
+	Run()
+	// uName, email, pass := "", "", ""
+	// mux := http.NewServeMux()
 
-	mux.HandleFunc("/singup", func(w http.ResponseWriter, r *http.Request) {
+	// mux.HandleFunc("/singup", func(w http.ResponseWriter, r *http.Request) {
 
-		r.ParseForm()
-		uName = r.FormValue("username")
-		email = r.FormValue("email")
-		pass = r.FormValue("pass")
-		uNameCheck := helper.IsEmpty(uName)
-		emaiCheck := helper.IsEmpty(email)
-		passCheck := helper.IsEmpty(pass)
-		if uNameCheck || emaiCheck || passCheck {
-			fmt.Fprintf(w, "Please fill all the fields")
-			return
-		}
-		fmt.Fprintf(w, "Welcome %s", uName)
+	// 	r.ParseForm()
+	// 	uName = r.FormValue("username")
+	// 	email = r.FormValue("email")
+	// 	pass = r.FormValue("pass")
+	// 	uNameCheck := helper.IsEmpty(uName)
+	// 	emaiCheck := helper.IsEmpty(email)
+	// 	passCheck := helper.IsEmpty(pass)
+	// 	if uNameCheck || emaiCheck || passCheck {
+	// 		fmt.Fprintf(w, "Please fill all the fields")
+	// 		return
+	// 	}
+	// 	fmt.Fprintf(w, "Welcome %s", uName)
 
-	})
+	// })
 
-	mux.HandleFunc("/login", func(w http.ResponseWriter, r *http.Request) {
-		r.ParseForm()
+	// mux.HandleFunc("/login", func(w http.ResponseWriter, r *http.Request) {
+	// 	r.ParseForm()
 
-		email = r.FormValue("email")
-		pass = r.FormValue("pass")
-		emailCheck := helper.IsEmpty(email)
-		pwdCheck := helper.IsEmpty(pass)
-		if emailCheck || pwdCheck {
-			fmt.Fprintf(w, "There is empty data")
-			return
-		}
-		dbEmail := "abc@com"
-		dbPass := "123"
-		if email == dbEmail && pass == dbPass {
-			fmt.Fprintf(w, "Login successful")
-		} else {
-			fmt.Fprintf(w, "Login failed")
-		}
-	})
-	http.ListenAndServe(":8080", mux)
+	// 	email = r.FormValue("email")
+	// 	pass = r.FormValue("pass")
+	// 	emailCheck := helper.IsEmpty(email)
+	// 	pwdCheck := helper.IsEmpty(pass)
+	// 	if emailCheck || pwdCheck {
+	// 		fmt.Fprintf(w, "There is empty data")
+	// 		return
+	// 	}
+	// 	dbEmail := "abc@com"
+	// 	dbPass := "123"
+	// 	if email == dbEmail && pass == dbPass {
+	// 		fmt.Fprintf(w, "Login successful")
+	// 	} else {
+	// 		fmt.Fprintf(w, "Login failed")
+	// 	}
+	// })
+	// http.ListenAndServe(":8080", mux)
 }
---
+++ webServer/webTable.go
@@ -0,0 +1,58 @@
+package webServer
+
+import (
+	"fmt"
+	"net/http"
+	"strconv"
+)
+
+type Human struct {
+	FName string
+	LName string
+	Age   int
+}
+
+func (hum Human) ServeHTTP(w http.ResponseWriter, r *http.Request) {
+
+	hum.FName = "Deneme"
+	hum.LName = "Surname"
+	hum.Age = 12
+
+	r.ParseForm()
+
+	fmt.Println(r.Form)
+	fmt.Fprint(w, "<table><tr><td><b>Ad</b></td><td><b>Soyad</b></td><td><b>Yaş</b></td></tr><tr><td>"+hum.FName+"</td><td>"+hum.LName+"</td><td>"+strconv.Itoa(hum.Age)+"</td></tr><tr></tr><tr></tr><tr><td><b>Path</b></td><td>"+r.URL.Path+"</td></tr></table>")
+
+	//fmt.Fprint(w, "<table>
+	// <tr>
+	//    <td><b>Ad</b></td>
+	//    <td><b>Soyad</b></td>
+	//    <td><b>Yaş</b></td>
+	// </tr>
+	// <tr>
+	//    <td>" + hum.FName + "</td>
+	//    <td>" + hum.LName + "</td>
+	//    <td>" + hum.Age + "</td>
+	// </tr>
+	// <tr> </tr>
+	// <tr> </tr>
+	// <tr>
+	//    <td> <b>Path</b> </td>
+	//    <td> "+ r.URL.Path +" </td>
+	// </tr>
+	// </table>")
+}
+
+func Run() {
+
+	var hum Human
+	err := http.ListenAndServe("localhost:8080", hum)
+
+	checkError(err)
+}
+
+func checkError(err error) {
+	if err != nil {
+		panic(err)
+	}
+}
